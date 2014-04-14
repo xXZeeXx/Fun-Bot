43ae01817c047ac26a70da8f2ea1b37bd2f8a946
Tawi T Jordan <aiseiab05@yahoo.com>
2014-04-14 02:47:39 -0700

    Update Fun.js

--- Bot/Fun.js
+++ Bot/Fun.js
@@ -530,6 +530,7 @@ function chatMe(msg)
                            API.moderateBanUser(users[i].username);
                           }
                         }
+                        }
                         break;
                
                 case "lock":
