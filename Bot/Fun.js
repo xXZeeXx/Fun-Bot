8fbeccd4fc50de7544a78b7dde87e20a29abaf72
Tawi T Jordan <aiseiab05@yahoo.com>
2014-04-14 03:51:52 -0700

    Update Fun.js

--- Bot/Fun.js
+++ Bot/Fun.js
@@ -457,12 +457,12 @@ function chatMe(msg)
                            API.sendChat("reward | reload | die | addsong | flipcoin | catfact | dogfact | hug | 8ball | punish | fortune | songlink | download | help | whywoot | whymeh | props | votes | woot | meh | skip | say | version | userstats | mystats | source");
                         }, 100);
                         setTimeout(function(){
-                           API.sendChat("addme | removeme | creator | status | bf | cf | tbf | tcf");
+                           API.sendChat("addme | removeme | creator | status | ban | bf | cf | tbf | tcf");
                         }, 600);
                         }else if(command[1].indexOf("@") > -1){
                             API.sendChat(command[1]+" My commands: reward | reload | die | addsong | flipcoin | catfact | dogfact | hug | 8ball | punish | fortune | songlink | download | help | whywoot | whymeh | props | votes | woot | meh | skip | say | version | userstats | mystats | source");
                         setTimeout(function(){
-                           API.sendChat("addme | removeme | creator | status | bf | cf | tbf | tcf");
+                           API.sendChat("addme | removeme | creator | status | ban | bf | cf | tbf | tcf");
                         }, 600);
                         }
                         break;
