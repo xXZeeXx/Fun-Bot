6d4e685e41838c6344a5e6e9783b6340c71ddbef
Tawi T Jordan <aiseiab05@yahoo.com>
2014-04-18 04:16:26 -0700

    Update Fun.js

--- Bot/Fun.js
+++ Bot/Fun.js
@@ -1308,7 +1308,7 @@ function chatMe(msg)
                 r = Math.floor(Math.random() * responses.length);
                 API.sendChat(responses[r].replace("{beggar}", data.from));
                 setTimeout(function(){
-                API.moderateBanUser(fromID, 0, API.BAN.HOUR);
+                API.moderateBanUser(fromID, "reason", API.BAN.HOUR);
                 }, 1500);
             }
             if(msg.indexOf(Funbot.filters.commandWords[i].toLowerCase()) > -1 && Funbot.settings.commandFilter){
